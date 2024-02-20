9144464ca5f68bcd163c56d139a8d97540816160
iamavu <imailavantika@gmail.com>
2024-02-20 10:47:59 +0530

    changed highlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
-      theme: "catppuccin macchiato",
+      theme: "nord",
       wrap: false
     }
   }

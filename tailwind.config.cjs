25f58650076302555e281cab56248e74b3ff02c5
iamavu <imailavantika@gmail.com>
2024-02-20 13:00:25 +0530

    font changes and post size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        mono: ["var(--font-jetbrains-mono)", ...defaultTheme.fontFamily.monospace],
+        mono: ["JetBrains Mono", monospace],
       },
     },
   },

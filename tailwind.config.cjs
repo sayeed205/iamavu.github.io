f631166a9a825b00fdd4808af0060380ca5bed94
iamavu <imailavantika@gmail.com>
2024-02-20 13:25:19 +0530

    font fix maybe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        mono: ["JetBrains Mono", "monospace"],
+        mono: ["var(--font-jetbrains-mono)", ...defaultTheme.fontFamily.mono],
       },
     },
   },
